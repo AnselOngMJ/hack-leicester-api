09c7d5c43d5fce4cf75aea7b03def9cf37d93a1c
AnselOngMJ <111511997+AnselOngMJ@users.noreply.github.com>
2024-02-11 08:31:45 +0000

    C:/Program Files/Git/ route shows all events

--- app.py
+++ app.py
@@ -1,7 +1,60 @@
-from flask import Flask
+import json
+from flask import Flask, jsonify, request
+import sqlite3
+import csv
 
 app = Flask(__name__)
 
-@app.route("/")
+def connect_to_db():
+    con = sqlite3.connect('database.db')
+    return con
+
+def create_db_table():
+    try:
+        con = connect_to_db()
+        con.execute('''
+            CREATE TABLE event (
+                id INT PRIMARY KEY,
+                name TEXT NOT NULL,
+                venue TEXT NOT NULL,
+                startDate TEXT NOT NULL,
+                endDate TEXT NOT NULL
+            );
+        ''')
+        con.commit()
+        print('Event table created successfully')
+    except:
+        print('Event table exists')
+    finally:
+        con.close()
+
+def format_event(result):
+    event = {
+        'id': result['id'],
+        'name': result['name'],
+        'venue': result['venue'],
+        'startDate': result['startDate'],
+        'endDate': result['endDate'],
+    }
+    return event
+
+@app.route('/')
 def hello_world():
-    return "<p>Hello, World!</p>"
+    create_db_table()
+    con = connect_to_db()
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+
+    with open('event.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            print(row)
+            cur.execute('INSERT INTO event VALUES (?, ?, ?, ?, ?)', list(row.values()))
+    
+    result = cur.execute('SELECT * FROM event')
+    events = [format_event(event) for event in result.fetchall()]
+    return jsonify(events)
+
+@app.route('/create')
+def create():
+    return
